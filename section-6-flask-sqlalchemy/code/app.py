2dbd12ee8bdd943b2c8a694b65020cf3d0ed8568
Luiz França <francaldr@gmail.com>
2020-03-29 02:04:03 -0300

    Making code deployable

--- section-6-flask-sqlalchemy/code/app.py
+++ section-6-flask-sqlalchemy/code/app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -9,7 +11,7 @@ from resources.store import Store, StoreList
 from resources.user import UserRegister
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'luiz'
 api = Api(app)
@@ -26,6 +28,7 @@ api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList, '/stores')
 api.add_resource(UserRegister, '/register')
 
+db.init_app(app)
+
 if __name__ == '__main__':
-    db.init_app(app)
     app.run(port=5000, debug=True)
